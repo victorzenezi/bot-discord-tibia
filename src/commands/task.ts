51087c1fee07b88010344e8d2c5c563e22e0dad9
Victor <victorzenezi@hotmail.com>
2020-08-05 19:43:54 -0300

    fix update manual xp

--- src/commands/task.ts
+++ src/commands/task.ts
@@ -1,20 +1,24 @@
 import { Message } from 'discord.js';
 import { Command } from './command';
 import { dailyTasks } from '../tasks/dailyExp';
+import { Xp } from '../commands/xp';
 
 export class Task implements Command {
 
     message: Message;
+    dailyXp: Xp;
 
     constructor(msg: Message){
         this.message = msg;
+        this.dailyXp = new Xp(msg);
     }
 
     public execute() {
         
     }
 
-    sendMsg(msg : Message){
+    async sendMsg(msg : Message){
+        await this.dailyXp.updateCharactersParty();
         this.startTask(msg);
     }
 
